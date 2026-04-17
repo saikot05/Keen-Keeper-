1e2bc0a4c32e2e50a12ff6094eb06ede49fde177
saikot05 <saikotislam08@gmail.com>
2026-04-17 19:10:01 +0600

    make responsive stats section

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -16,24 +16,24 @@ const Stats = () => {
   { name: 'Video', value: videoCount,fill:"#3DAB6A" },
 ];
     return (
-        <div className='container mx-auto flex flex-col justify-center my-10'>
-            <h2 className='font-semibold text-3xl mb-16 text-center'>Friendship Analytics</h2>
-            <div className='flex justify-center shadow-sm p-5 border-slate-300'>
+        <div className='container mx-auto px-4 my-6 md:my-10'>
+            <h2 className='font-semibold text-2xl md:text-3xl mb-8 md:mb-16 text-center'>Friendship Analytics</h2>
+            <div className='flex flex-col md:flex-row justify-center shadow-sm p-3 md:p-5 border-slate-300 rounded-lg'>
                 <p className='text-xl font-semibold text-[#244D3F] mb-5'>By Interaction Type</p>
                 <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
-            <Pie
-                data={data}
-                innerRadius="80%"
-                outerRadius="100%"
-                cornerRadius="50%"
-                fill="#8884d8"
-                paddingAngle={5}
-                dataKey="value"
-                isAnimationActive={true}
-            />
-            <Legend />
-            <Tooltip />
-            </PieChart>
+                    <Pie
+                            data={data}
+                            innerRadius="80%"
+                            outerRadius="100%"
+                            cornerRadius="50%"
+                            fill="#8884d8"
+                            paddingAngle={5}
+                            dataKey="value"
+                            isAnimationActive={true}
+                    />
+                    <Legend />
+                    <Tooltip />
+                </PieChart>
             </div>
         </div>
     );
